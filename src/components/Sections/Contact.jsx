e1813ec23599fbd463ab50dc64aa49c37a4cadc7
najubudeen <najubudeenktm@gmail.com>
2025-06-27 12:15:26 +0530

    feat:images added

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { FaPhoneVolume } from "@react-icons/all-files/fa/FaPhoneVolume"
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub"
-import { FaLinkedinIn } from "@react-icons/all-files/fa/FaLinkedinIn"
 import { FiMail } from "@react-icons/all-files/fi/FiMail"
 import { Link as SocialMediaContact } from 'react-router-dom'
 
@@ -104,36 +103,7 @@ function Contact() {
                 <FaGithub className='text-accent hover:text-accent-secondary h-7'/> 
 
             </SocialMediaContact>
-            <SocialMediaContact to={"https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"} onClick={handleLinkClick}>
-                <FaLinkedinIn className='text-accent hover:text-accent-secondary h-7'/> 
-
-            </SocialMediaContact>
         </div>
-        {/* <div className='flex flex-Col items-center justify-center w-full'>
-            <span className='text-defaultBase md:px-2 sm:text-smallText md:text-midText lg:text-largeText font-montserrat'>
-                Or
-            </span>
-        </div>
-
-        <div className="flex flex-col">
-                <h1 className="text-smName md:text-mdName font-ralway pb-5">Send Me An Email</h1>
-            </div>
-
-        <div className="flex flex-col md:flex-row w-full">
-            <div className="flex gap-4 md:gap-9 flex-col w-full md:w-1/2 pr-0 md:pr-2">
-                <input type="text" className='pl-2 md:pl-3 text-defaultBase px-2 md:px-2 sm:text-smallText md:text-midText lg:text-largeText font-montserrat' style={{ height: '50px',border: '1px solid #E1E1E1',borderRadius:'5px'}} placeholder='name' />
-                <input type="email" className='pl-2 md:pl-3 text-defaultBase px-2 md:px-2 sm:text-smallText md:text-midText lg:text-largeText font-montserrat' style={{ height: '50px',border: '1px solid #E1E1E1',borderRadius:'5px'}} placeholder='email' />
-            </div>
-            <div className="flex flex-col pt-4 md:pt-0 w-full md:w-1/2 md:pl-2">
-                <textarea name="" className='pl-3 pt-1 text-defaultBase px-2 md:px-2 sm:text-smallText md:text-midText lg:text-largeText font-montserrat' style={{ border: '1px solid #E1E1E1',borderRadius:'5px'}} id="" placeholder='Message' cols="10" rows="5"></textarea>
-            </div>
-        </div>
-        <div className="flex flex-col items-center justify-center md:items-end md:justify-end w-full">
-        <button className="bg-accent hover:bg-accent-secondary py-1 px-3 text-defaultBase sm:text-smallText md:text-midText lg:text-largeText font-montserrat">
-            Send Email
-        </button>
-        </div> */}
-
     </div>
   )
 }
--- src/components/Sections/Project.jsx
+++ src/components/Sections/Project.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import NavBarProject from "../NavBarProject";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import datas from "../../datas/data.json";
@@ -14,10 +13,10 @@ function Projects() {
   return (
     <>
       <NavBarProject />
-      <div className="w-full bg-secondary-light ">
-        <div className="container mx-auto">
+      <div className="w-full bg-secondary-light">
+        <div className="container mx-auto py-8">
           <h1 className="text-smName md:text-mdName font-ralway px-4 md:px-0 py-6">
-            Construction Site
+            Project Details
           </h1>
 
           <div className="flex flex-col md:flex-row">
@@ -38,9 +37,6 @@ function Projects() {
                 })}
               </div>
               <div className="w-full flex-col md:w-5/12">
-                {/* <h3 className="text-smProjectsecondaryHeading font-ralway w-full px-12 py-4">
-                  Color Scheme
-                </h3> */}
                 {datas.map((data, index) => {
                   if (data.id == id) {
                     return (
@@ -48,52 +44,140 @@ function Projects() {
                         {data.properties.map((dataProps) => {
                           return (
                             <>
-                            <div className="pb-0 md:pb-4">
-                              <h3 className="text-smProjectsecondaryHeading font-ralway w-full px-12 py-4">
-                                Color Scheme
-                              </h3>
-                              {
-                                dataProps.colorscheme == "" ? <div className="flex pl-4 md:pl-16">Color Scheme Not Available</div> : <span className="flex pl-4 md:pl-16"><img src={dataProps.colorscheme} className="w-1/2 h-[40px]" /></span>
-                              }
-                              
-                            </div>
-                            <div className="pb-0 md:pb-4">
-                              <h3 className="text-smProjectsecondaryHeading font-ralway w-full px-12 py-4">
-                                Fonts
-                              </h3>
-                              {
-                                dataProps.font == "" ? <span className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">Font Not Availeble</span> : <span className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">{dataProps.font}</span>
-                              }
-                              
-                            </div>
-                            <div className="pb-0 md:pb-4">
-                              <h3 className="text-smProjectsecondaryHeading font-ralway w-full px-12 py-4">
-                                Live Preview
-                              </h3>
-                                  { dataProps.preview == "" ? <div className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">Not Available for now</div> : <Link to="/" className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat" target="_blank" >View Site</Link> }
-                                  
-                            </div>
+                              {dataProps.type == "" ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    Type
+                                  </h3>
+                                  <span className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">
+                                    {dataProps.type}
+                                  </span>
+                                </div>
+                              )}
 
-                            <div className="pb-0 md:pb-4">
-                              <h3 className="text-smProjectsecondaryHeading font-ralway w-full px-12 py-4">
-                                PROJECT FILES
-                              </h3>
-                              <div className="flex flex-col items-start pl-4 md:pl-12 gap-2">
-                                {
-                                  dataProps.designtemplate == "" ? <span className=""></span> : dataProps.designtemplate
-                                }
-                                {
-                                  dataProps.wp == "" ? <span className=""></span> : dataProps.wp
-                                }
-                                {
-                                  dataProps.react == "" ? <span className=""></span> : dataProps.react
-                                }
-                                {
-                                  dataProps.htmltemplate == "" ? <span className=""></span> : dataProps.htmltemplate
-                                }
+                              {dataProps.colorscheme == "" ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    Color Scheme
+                                  </h3>
+                                  <span className="flex pl-4 md:pl-16">
+                                    <img
+                                      src={dataProps.colorscheme}
+                                      className="w-1/2 h-[40px]"
+                                    />
+                                  </span>
+                                </div>
+                              )}
+                              {dataProps.font == "" ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    Typeface
+                                  </h3>
 
-                              </div>
-                            </div>
+                                  <span className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">
+                                    {dataProps.font}
+                                  </span>
+                                </div>
+                              )}
+
+                              {dataProps.width == "" ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    Width
+                                  </h3>
+                                  <span className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">
+                                    {dataProps.width}
+                                  </span>
+                                </div>
+                              )}
+
+                              {dataProps.contentWidth == "" ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    Content Width
+                                  </h3>
+                                  <span className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">
+                                    {dataProps.contentWidth}
+                                  </span>
+                                </div>
+                              )}
+
+                              {dataProps.wpTheme == "" ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    Wordpress Theme
+                                  </h3>
+                                  {
+                                    <span className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat">
+                                      {dataProps.wpTheme}
+                                    </span>
+                                  }
+                                </div>
+                              )}
+
+                              {dataProps.wpPluginsUsed.length === 0 ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    WordPress Plugins Used
+                                  </h3>
+                                  {dataProps.wpPluginsUsed.map((itms, ind) => {
+                                    return (
+                                      <ul className="list-disc px-24 py-2">
+                                        <li>{itms}</li>
+                                      </ul>
+                                    );
+                                  })}
+                                </div>
+                              )}
+
+                              {
+                                dataProps.libraries.length === 0 ? (
+                                    <span></span>
+                                ):(
+                                    <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    React Libraries Used
+                                  </h3>
+                                  {dataProps.libraries.map((itms, ind) => {
+                                    return (
+                                      <ul className="list-disc px-24 py-2">
+                                        <li>{itms}</li>
+                                      </ul>
+                                    );
+                                  })}
+                                </div>
+                                )
+                              }
+                              {dataProps.preview == "" ? (
+                                <span></span>
+                              ) : (
+                                <div className="pb-0 md:pb-4">
+                                  <h3 className="text-smProjectsecondaryHeading font-ralway font-medium w-full px-12 py-4">
+                                    Live Preview
+                                  </h3>
+                                  <Link
+                                    to="/"
+                                    className="pl-4 md:pl-16 text-defaultBase px-8 md:px-8 sm:text-smallText md:text-midText lg:text-largeText font-montserrat"
+                                    target="_blank"
+                                  >
+                                    View Site
+                                  </Link>
+                                </div>
+                              )}
                             </>
                           );
                         })}
