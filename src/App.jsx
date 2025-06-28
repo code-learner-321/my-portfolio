5ded2d2336581aac526aebb11cb00b66aef2905d
najubudeen <najubudeenktm@gmail.com>
2025-06-28 13:06:10 +0530

    deleted yml file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Sections/Home";
 import Project from "./components/Sections/Project";
 import ScrollToElement from "./ScrollToElement";
