2e89cfceef496ad4f7a1207b1dabbb28d39f4142
najubudeen <najubudeenktm@gmail.com>
2025-06-28 13:17:54 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import "./App.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Sections/Home";
 import Project from "./components/Sections/Project";
 import ScrollToElement from "./ScrollToElement";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-
   return (
     <>
-      <Router basename={import.meta.env.VITE_BASE_URL || '/'}>
-        <ScrollToTop/>
-        <ScrollToElement/>
+      <Router basename="/my-portfolio">
+        <ScrollToTop />
+        <ScrollToElement />
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/project" element={<Project />} />
         </Routes>
       </Router>
