db473414bda92eacc5525da4ef39c9c7e8e7c87e
najubudeen <najubudeenktm@gmail.com>
2025-06-28 12:36:30 +0530

    fixed baurl

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      <Router basename={import.meta.env.VITE_BASE_URL || '/'}>
         <ScrollToTop/>
         <ScrollToElement/>
         <Routes>
